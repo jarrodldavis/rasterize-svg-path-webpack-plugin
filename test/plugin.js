58fe1d5598b50139343cba49db46bafd86c81eb9
Jarrod Davis <developer@jarrodldavis.com>
2019-02-09 02:17:15 -0700

    Add tests for webpack plugin
    - Install `sinon`, `sinon-should`, and `proxyquire`
    - Globally require `sinon-should` when running tests with `mocha`
    - Configure sinon to automatically restore fakes after each test
    - Install `webpack`
    - Add test to ensure the plugin taps into webpack's `emit` hook
    - Add test to ensure the plugin correcly uses `Rasterizer` and adds a
      `RasterImageWebpackAsset` to the compilation

---
+++ test/plugin.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+const sinon = require('sinon');
+const proxyquire = require('proxyquire').noPreserveCache();
+const webpack = require('webpack');
+
+const Rasterizer = require('../rasterizer');
+const RasterImageWebpackAsset = require('../asset');
+
+const {
+  INPUT_WIDTH,
+  INPUT_HEIGHT,
+  INPUT_PATH,
+  STROKE_COLOR,
+  FILL_COLOR
+} = require('./_constants');
+
+const OUTPUT = {
+  filePath: 'images/icon-128.png',
+  format: 'png',
+  width: 128,
+  height: 128
+};
+
+const WEBPACK_CONFIG = {
+  context: path.join(__dirname, './app-fixture'),
+  entry: './app.js',
+  output: {
+    path: path.join(__dirname, './app-fixture-dist')
+  }
+};
+
+describe('webpack plugin', function() {
+  let rasterizeBuffer;
+  let RasterizeSvgPathWebpackPlugin;
+  let stubRasterizer;
+
+  beforeEach(function() {
+    rasterizeBuffer = Buffer.from([]);
+    stubRasterizer = sinon.createStubInstance(Rasterizer);
+    stubRasterizer.rasterize.returns(rasterizeBuffer);
+
+    RasterizeSvgPathWebpackPlugin = proxyquire('../', {
+      './rasterizer': function MockRasterizer() {
+        return stubRasterizer;
+      }
+    });
+  });
+
+  it('taps into compiler emit hook', function() {
+    const compiler = webpack(WEBPACK_CONFIG);
+
+    const plugin = new RasterizeSvgPathWebpackPlugin(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, FILL_COLOR, [OUTPUT]);
+    sinon.stub(compiler.hooks.emit, 'tapPromise');
+    plugin.apply(compiler);
+
+    compiler.hooks.emit.tapPromise
+            .should.have.been.calledOnce()
+            .and.calledWithMatch(plugin.constructor.name, sinon.match.func);
+  });
+
+  it('adds output buffer asset to compilation', async function() {
+    const compilation = {
+      assets: {}
+    };
+
+    const plugin = new RasterizeSvgPathWebpackPlugin(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, FILL_COLOR, [OUTPUT]);
+    await plugin.emit(compilation);
+
+    stubRasterizer.rasterize.should.have.been.calledOnce()
+                            .and.calledWithExactly(OUTPUT.format, OUTPUT.width, OUTPUT.height);
+
+    compilation.assets
+               .should.have.a.property(OUTPUT.filePath)
+               .which.is.an.instanceof(RasterImageWebpackAsset)
+               .and.has.a.property('buffer')
+               .which.is.exactly(rasterizeBuffer);
+  });
+})
