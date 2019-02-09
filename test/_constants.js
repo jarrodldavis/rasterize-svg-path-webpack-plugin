395fc14cd81a9de21f2b4c5b4f5a2157679e2729
Jarrod Davis <developer@jarrodldavis.com>
2019-02-09 00:53:55 -0700

    Extract test constants
    Extract constant values used in the `Rasterizer` test into a separate module.

---
+++ test/_constants.js
@@ -0,0 +1,11 @@
+const { mdiCheckCircle } = require('@mdi/js');
+
+module.exports = {
+  INPUT_WIDTH: 24,
+  INPUT_HEIGHT: 24,
+  INPUT_PATH: mdiCheckCircle,
+  STROKE_COLOR: 'green',
+  FILL_COLOR: 'red',
+  OUTPUT_WIDTH: 96,
+  OUTPUT_HEIGHT: 96,
+};
--- test/rasterizer.js
+++ test/rasterizer.js
@@ -5,21 +5,23 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
 const Rasterizer = require('../rasterizer');
-const { mdiCheckCircle } = require('@mdi/js');
 
-const INPUT_WIDTH = 24;
-const INPUT_HEIGHT = 24;
-const STROKE_COLOR = 'green';
-const FILL_COLOR = 'red';
-const OUTPUT_WIDTH = 96;
-const OUTPUT_HEIGHT = 96;
+const {
+  INPUT_WIDTH,
+  INPUT_HEIGHT,
+  INPUT_PATH,
+  STROKE_COLOR,
+  FILL_COLOR,
+  OUTPUT_WIDTH,
+  OUTPUT_HEIGHT
+} = require('./_constants');
 
 describe('Rasterizer', function() {
   describe('png', function() {
     it('renders correctly with stroke color', async function() {
       const expected = await readFile(path.join(__dirname, './reference-images/stroke-only.png'));
 
-      const rasterizer = new Rasterizer(mdiCheckCircle, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, null);
+      const rasterizer = new Rasterizer(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, null);
       const actual = await rasterizer.rasterize('png', OUTPUT_WIDTH, OUTPUT_HEIGHT);
 
       actual.should.pixelmatch(expected);
@@ -28,7 +30,7 @@ describe('Rasterizer', function() {
     it('renders correctly with fill color', async function() {
       const expected = await readFile(path.join(__dirname, './reference-images/fill-only.png'));
 
-      const rasterizer = new Rasterizer(mdiCheckCircle, INPUT_WIDTH, INPUT_HEIGHT, null, FILL_COLOR);
+      const rasterizer = new Rasterizer(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, null, FILL_COLOR);
       const actual = await rasterizer.rasterize('png', OUTPUT_WIDTH, OUTPUT_HEIGHT);
 
       actual.should.pixelmatch(expected);
@@ -37,7 +39,7 @@ describe('Rasterizer', function() {
     it('renders correctly with fill and stroke color', async function() {
       const expected = await readFile(path.join(__dirname, './reference-images/stroke-and-fill.png'));
 
-      const rasterizer = new Rasterizer(mdiCheckCircle, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, FILL_COLOR);
+      const rasterizer = new Rasterizer(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, FILL_COLOR);
       const actual = await rasterizer.rasterize('png', OUTPUT_WIDTH, OUTPUT_HEIGHT);
 
       actual.should.pixelmatch(expected);
