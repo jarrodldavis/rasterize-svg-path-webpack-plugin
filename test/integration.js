397c31eec017dc8f8b61b3e16ec84eb53756b023
Jarrod Davis <developer@jarrodldavis.com>
2019-02-09 13:28:37 -0700

    Add integration test for multiple outputs
    - Add 48x48 reference images
    - Add integration test for a webpack build with multiple outputs

--- test/integration.js
+++ test/integration.js
@@ -18,6 +18,11 @@ const {
 
 const OUTPUT_FORMAT = 'png';
 
+const REFERENCE_PATH_48 = path.join(__dirname, './reference-images/stroke-and-fill-48.png');
+const OUTPUT_PATH_48 = 'images/icon-48.png';
+const OUTPUT_WIDTH_48 = 48;
+const OUTPUT_HEIGHT_48 = 48;
+
 const REFERENCE_PATH_96 = path.join(__dirname, './reference-images/stroke-and-fill-96.png');
 const OUTPUT_PATH_96 = 'images/icon-96.png';
 const OUTPUT_WIDTH_96 = 96;
@@ -86,5 +91,25 @@ describe('integration', function() {
 
     outputfs.should.have.file(path.join(config.output.path, OUTPUT_PATH_96))
             .which.pixelmatches(reference);
+  });
+
+  it('emits multiple output files', async function() {
+    const reference48 = await readFile(REFERENCE_PATH_48);
+    const reference96 = await readFile(REFERENCE_PATH_96);
+
+    config.plugins.push(
+      new RasterizeSvgPathWebpackPlugin(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, FILL_COLOR, [
+        { filePath: OUTPUT_PATH_48, format: OUTPUT_FORMAT, width: OUTPUT_WIDTH_48, height: OUTPUT_HEIGHT_48 },
+        { filePath: OUTPUT_PATH_96, format: OUTPUT_FORMAT, width: OUTPUT_WIDTH_96, height: OUTPUT_HEIGHT_96 }
+      ])
+    );
+
+    await run();
+
+    outputfs.should.have.file(path.join(config.output.path, OUTPUT_PATH_48))
+            .which.pixelmatches(reference48);
+
+    outputfs.should.have.file(path.join(config.output.path, OUTPUT_PATH_96))
+            .which.pixelmatches(reference96);
   });
 });
