8587349bb5bd78dc5510f37caba2e005a03037b5
Jarrod Davis <developer@jarrodldavis.com>
2019-02-09 13:14:38 -0700

    Add integration test
    - Add filesystem assertions to `should`
    - Add integration that tests a webpack build with a single input and
      output

--- test/_assertions.js
+++ test/_assertions.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+
+const MemoryFS = require('memory-fs');
 const pixelmatch = require('pixelmatch');
 const PNG = require('pngjs').PNG;
 
@@ -38,5 +41,39 @@ should.use(function(should, Assertion) {
 
     let mismatchedPixelCount = pixelmatch(actual.data, expected.data, null, width, height, { threshold: 0.15 });
     mismatchedPixelCount.should.equal(0, 'All pixels should match.');
+  });
+
+  Assertion.alias('pixelmatch', 'pixelmatches');
+});
+
+should.use(function(should, Assertion) {
+  Assertion.add('path', function(expectedPath) {
+    this.params = { operator: 'to have path', expected: expectedPath };
+
+    if (this.obj !== fs && !(this.obj instanceof MemoryFS)) {
+      throw new TypeError('This assertion can only be used on `fs` or an instance of `memory-fs`.');
+    }
+
+    if (typeof expectedPath !== 'string') {
+      throw new TypeError('The expected path must be a string.');
+    }
+
+    if (!this.obj.existsSync(expectedPath)) {
+      throw new should.AssertionError({ message: `Path '${expectedPath}' does not exist.` })
+    }
+  });
+
+  Assertion.add('file', function(expectedPath) {
+    this.params = { operator: 'to have a file at path', expected: expectedPath };
+
+    this.obj.should.have.path(expectedPath);
+
+    const stat = this.obj.statSync(expectedPath);
+    if (!stat.isFile()) {
+      throw new should.AssertionError({ message: `Path '${expectedPath}' is not a file.` });
+    }
+
+    const buffer = this.obj.readFileSync(expectedPath);
+    this.obj = buffer;
   });
 });
--- test/integration.js
+++ test/integration.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
+const path = require('path');
 const util = require('util');
 
 const webpack = require('webpack');
 const MemoryFS = require('memory-fs');
 const { errorFromList } = require('verror')
+
+const RasterizeSvgPathWebpackPlugin = require('../');
+
+const {
+  INPUT_WIDTH,
+  INPUT_HEIGHT,
+  INPUT_PATH,
+  STROKE_COLOR,
+  FILL_COLOR,
+  OUTPUT_HEIGHT,
+  OUTPUT_WIDTH
+} = require('./_constants');
+
+const readFile = util.promisify(fs.readFile);
 
 describe('integration', function() {
   this.slow(500);
@@ -50,5 +66,22 @@ describe('integration', function() {
 
   it('test harness works', async function() {
     await run();
+  });
+
+  it('emits single output file', async function() {
+    const reference = await readFile(path.join(__dirname, './reference-images/stroke-and-fill.png'));
+
+    const filePath = 'images/icon-96.png';
+
+    config.plugins.push(
+      new RasterizeSvgPathWebpackPlugin(INPUT_PATH, INPUT_WIDTH, INPUT_HEIGHT, STROKE_COLOR, FILL_COLOR, [
+        { filePath, format: 'png', width: OUTPUT_WIDTH, height: OUTPUT_HEIGHT }
+      ])
+    );
+
+    await run();
+
+    outputfs.should.have.file(path.join(config.output.path, filePath))
+            .which.pixelmatches(reference);
   });
 });
